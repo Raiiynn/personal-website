2019e843afabe00a265c58a59fe7f7f593b36014
Raiiynnn <githubdaffa@gmail.com>
2025-11-22 21:34:04 +0700

    feat: fix error and add placeholder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)', 'sans-serif'],
+        sans: ['Space Grotesk', 'sans-serif'],
         headline: ['Space Grotesk', 'sans-serif'],
         code: ['monospace'],
       },
